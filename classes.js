d5878ba59f5a9201e4216ae21d1e6b937b675e1d
Rob <superserva@gmail.com>
2017-03-25 11:21:33 +1300

    add blue and green dots with JS

--- classes.js
+++ classes.js
@@ -1,12 +1,12 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+   one()
+   two()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -16,6 +16,12 @@ function one () {
 
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
+}
+
+function two() {
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
 }
 
 // CREATE FUNCTION two HERE
